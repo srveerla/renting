8065e9b831e6fc8b41ff7d5a1087d76a4bc5f58c
srveerla <39939637+srveerla@users.noreply.github.com>
2023-11-10 19:38:36 +0530

    Update renting.py

--- renting.py
+++ renting.py
@@ -1,43 +1,30 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rentings.db'
-db = SQLAlchemy(app)
-
-class Renting(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    book_id = db.Column(db.Integer, nullable=False)
-    start_date = db.Column(db.String(20), nullable=False)
-    end_date = db.Column(db.String(20), nullable=False)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/rentings_db'
+mongo = PyMongo(app)
 
 @app.route('/rentings', methods=['GET'])
 def get_rentings():
-    rentings = Renting.query.all()
-    return jsonify({"rentings": [{"id": renting.id, "user_id": renting.user_id, "book_id": renting.book_id, "start_date": renting.start_date, "end_date": renting.end_date} for renting in rentings]})
+    rentings = list(mongo.db.rentings.find())
+    return jsonify({"rentings": rentings})
 
 @app.route('/rentings', methods=['POST'])
 def create_renting():
     data = request.get_json()
 
-    # Validate user
-    user_id = data.get('userId')
-    if not User.query.get(user_id):
-        return jsonify({"error": "User not found"}), 404
+    # Assuming the data structure, adjust as needed
+    renting_record = {
+        "userId": data.get("userId"),
+        "bookId": data.get("bookId"),
+        "startDate": data.get("startDate"),
+        "endDate": data.get("endDate"),
+    }
 
-    # Validate book
-    book_id = data.get('bookId')
-    if not Book.query.get(book_id):
-        return jsonify({"error": "Book not found"}), 404
+    mongo.db.rentings.insert_one(renting_record)
 
-    # Create renting record
-    renting_record = Renting(user_id=user_id, book_id=book_id, start_date=data.get("startDate"), end_date=data.get("endDate"))
-    db.session.add(renting_record)
-    db.session.commit()
-
-    return jsonify({"renting": {"id": renting_record.id, "user_id": renting_record.user_id, "book_id": renting_record.book_id, "start_date": renting_record.start_date, "end_date": renting_record.end_date}}), 201
+    return jsonify({"renting": renting_record}), 201
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(port=5002)
